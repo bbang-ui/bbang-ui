4b7d4084c6f641cebf40698f8b0c3d51571260bc
oauch <lgm9515@naver.com>
2024-04-01 15:30:25 +0900

    ✏️ [#4] Feat: 컬러 상수화

---
+++ src/styles/color.ts
@@ -0,0 +1,13 @@
+const COLORS = {
+  // 기본 색상
+  primary: '#F0E7DB',
+  black: '#000',
+  white: '#FFF',
+
+  // 빵빵이 색상
+  skin: '#F5DDC2',
+  tShirts: '#01641D',
+  mouse: '#F3C0B9',
+};
+
+export default COLORS;
