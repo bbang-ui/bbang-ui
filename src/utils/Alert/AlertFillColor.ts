af07f5e829a5936c8abd567b2d24f99aa20a0726
oauch <lgm9515@naver.com>
2024-04-12 21:14:21 +0900

    ✏️ [#15] Feat: Alert Fill 컬러 유틸

---
+++ src/utils/Alert/AlertFillColor.ts
@@ -0,0 +1,19 @@
+import COLORS from '@/styles/color';
+import { AlertProps } from '@/types/alert';
+
+function AlertFillColor({ status }: AlertProps) {
+  switch (status) {
+    case 'success':
+      return COLORS.FILL_SUCCESS;
+    case 'error':
+      return COLORS.FILL_ERROR;
+    case 'warning':
+      return COLORS.FILL_WARNING;
+    case 'info':
+      return COLORS.FILL_INFO;
+    default:
+      return COLORS.primary;
+  }
+}
+
+export default AlertFillColor;
