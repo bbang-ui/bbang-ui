46f9e170f45277679864d46707391cd0d8f98c22
oauch <lgm9515@naver.com>
2024-04-16 19:25:57 +0900

    💩 [#20] Chore: 주석 수정

--- src/utils/compact.ts
+++ src/utils/compact.ts
@@ -5,9 +5,6 @@
  * 부모 컴포넌트 -> props.children으로 props를 넘겨줄 때 기존 children props 내용이 아닌
  * 매개변수로 넘겨준 객체로 props를 커스텀 해주는 함수 입니다.
  *
- * [With Function]
- * childrenWithProps
- *
  * @param object
  * @returns object
  */
