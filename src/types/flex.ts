8d2988b80ecbd98fb306eed55a1d75f1bd488cec
박종민 <qooktree@naver.com>
2024-04-28 21:37:31 +0900

    ✏️ [#26] Feat: Flex Layout 타입 설정

---
+++ src/types/flex.ts
@@ -0,0 +1,36 @@
+import { DefaultProps } from '@/types/default';
+
+type JustifyContentToken =
+  | 'center'
+  | 'flex-end'
+  | 'flex-start'
+  | 'space-around'
+  | 'space-between'
+  | 'space-evenly'
+  | 'stretch'
+  | 'normal';
+
+type AlignItemsToken =
+  | 'baseline'
+  | 'center'
+  | 'flex-end'
+  | 'flex-start'
+  | 'stretch'
+  | 'normal';
+
+type FlexDirectionToken = 'column' | 'column-reverse' | 'row' | 'row-reverse';
+
+type FlexWrapToken = 'nowrap' | 'wrap' | 'wrap-reverse';
+
+interface FlexProps extends DefaultProps {
+  direction?: FlexDirectionToken;
+  justify?: JustifyContentToken;
+  align?: AlignItemsToken;
+  wrap?: FlexWrapToken;
+  grow?: number;
+  shrink?: number;
+  basis?: string;
+  gap?: number;
+}
+
+export type { FlexProps };
